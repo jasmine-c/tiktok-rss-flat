2327ae07802a4a02398e38782b74b264f3735fc4
Jasmine Cohen <17069980+jasmine-c@users.noreply.github.com>
2025-12-26 16:52:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
-ghPagesURL = "https://github.com/jasmine-c/tiktok-rss-flat/"
+ghPagesURL = "https://jasmine-c.github.io/tiktok-rss-flat/"
 ghRawURL = "https://raw.githubusercontent.com/jasmine-c/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
